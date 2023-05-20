728eae03d4b110dc6f54258670e57caa9625bebf
FiftyFrick <walter-541@hotmail.com>
2023-05-20 17:12:42 -0300

    agregue la logica de la base de datos y agregue formato al inicio de sesion, ademas dejo preparado la pantalla de registro

---
+++ logica/conexion.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+
+$host = "localhost";
+$usuario= "root";
+$clave= "";
+$bd= "efecto4t";
+
+$conexion= mysqli_connect($host,$usuario,$clave,$bd);
+/*
+if($conexion){
+    echo "conectado correctamente";
+
+}else{
+    echo"no se pudo conectar";
+}
+
+*/
+?>
+
