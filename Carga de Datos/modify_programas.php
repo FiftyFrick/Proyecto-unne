2b6cc86a9cf6046eae3f0ab6a269155fdfeee68b
walter <walter-541@hotmail.com>
2023-11-09 16:54:56 -0300

    arreglando insert documentos

--- Carga de Datos/modify_programas.php
+++ Carga de Datos/modify_programas.php
@@ -32,18 +32,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if ($extension == "pdf") {
                 // Mueve el archivo a la carpeta destino
                 if (move_uploaded_file($_FILES['documento']['tmp_name'], $carpeta_destino . $nombre_archivo)) {
-                    // Insertar en la base de datos
-                    $insertPrograma = "INSERT INTO programas (id_asignatura, id_carrera, id_plan, cuatrimestre, responsable, resolucion_CD, fecha_resolucion, archivo_PDF)
+                    
+                    // ACTUALIZAR en la base de datos
+                    
+                    $updatePrograma = "INSERT INTO programas (id_asignatura, id_carrera, id_plan, cuatrimestre, responsable, resolucion_CD, fecha_resolucion, archivo_PDF)
                                        VALUES ('$asignatura', '$nombre_carrera', '$nombre_plan', '$cuatrimestre', '$responsable', '$resolucion_CD', '$fecha_resolucion', '$nombre_archivo')";
-                    $resultado = mysqli_query($conn, $insertPrograma);
+                    // $resultado = mysqli_query($conn, $updatePrograma);
 
-                    if ($resultado) {
-                        // Redirigir de vuelta a la página anterior con un mensaje de éxito
-                        header("Location: {$_SERVER['HTTP_REFERER']}?mensaje=exito");
-                        exit(); // Asegurar que el script se detenga después de redirigir
-                    } else {
-                        echo "Error al insertar el programa: " . mysqli_error($conn);
-                    }
+                    // if ($resultado) {
+                    //     // Redirigir de vuelta a la página anterior con un mensaje de éxito
+                    //     header("Location: {$_SERVER['HTTP_REFERER']}?mensaje=exito");
+                    //     exit(); // Asegurar que el script se detenga después de redirigir
+                    // } else {
+                    //     echo "Error al insertar el programa: " . mysqli_error($conn);
+                    // }
                 } else {
                     echo "Error al mover el archivo a la carpeta destino.";
                 }
@@ -57,19 +59,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             $insertPrograma = "INSERT INTO programas (id_asignatura, id_carrera, id_plan, cuatrimestre, responsable, resolucion_CD, fecha_resolucion)
                             VALUES ('$asignatura', '$nombre_carrera', '$nombre_plan', '$cuatrimestre', '$responsable', '$resolucion_CD', '$fecha_resolucion')";
-                    $resultado = mysqli_query($conn, $insertPrograma);
+                    // $resultado = mysqli_query($conn, $insertPrograma);
 
-                    if ($resultado) {
-                        // Redirigir de vuelta a la página anterior con un mensaje de éxito
-                        header("Location: {$_SERVER['HTTP_REFERER']}?mensaje=exito");
+                    // if ($resultado) {
+                    //     // Redirigir de vuelta a la página anterior con un mensaje de éxito
+                    //     header("Location: {$_SERVER['HTTP_REFERER']}?mensaje=exito");
                         
                         
-                    } else {
-                        echo "Error al insertar el programa: " . mysqli_error($conn);
-                    }exit(); // Asegurar que el script se detenga después de redirigir
+                    // } else {
+                    //     echo "Error al insertar el programa: " . mysqli_error($conn);
+                    // }exit(); // Asegurar que el script se detenga después de redirigir
         }
     }
 } else {
     echo "La solicitud no es de tipo POST.";
 }
+
+// if (!empty($IdPlanForm)) {
+//     $updateQuery = "UPDATE plan_de_estudio SET 
+
+//         nombre_plan = '" . $datos["Nombre del Plan"] . "',
+//         id_carrera = '" . $datos["Nombre de Carrera"] . "',
+//         fecha_inicio = '" . $datos["Fecha de Inicio"] . "',
+//         fecha_fin = '" . $datos["Fecha de Fin"] . "',
+//         res_cd = '" . $datos["Resolución CD"] . "',
+//         res_sd = '" . $datos["Resolución SD"] . "',
+//         res_coneau = '" . $datos["Resolución CONEAU"] . "',
+//         res_modif = '" . $datos["Resolución Modificada"] . "'
+        
+//     WHERE id_plan = $IdPlanForm";
+// }
+    
+// $updateResult = mysqli_query($conn, $updateQuery);
+
 ?>
