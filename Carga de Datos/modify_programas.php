238c989c0a249b04214990924534868a7628b2c4
walter <walter-541@hotmail.com>
2023-11-23 17:00:17 -0300

    detalles

--- Carga de Datos/modify_programas.php
+++ Carga de Datos/modify_programas.php
@@ -7,26 +7,81 @@ $IdProgramaForm = $_POST["id_programa"];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['asignatura'])) {
-        // Formulario de planes de estudio
+        // Formulario de Programas
         $asignatura = $_POST['asignatura'];
-        $nombre_plan = $_POST['nombre_plan']; //necesito que sea un int "id_plan"
-        $nombre_carrera = $_POST['nombre_carrera']; //necesito que sea un int "id_carrera"
+        $nombre_plan = $_POST['nombre_plan']; // "id_plan"
+        $nombre_carrera = $_POST['nombre_carrera']; // "id_carrera"
         $cuatrimestre = $_POST['cuatrimestre'];
         $responsable = $_POST['responsable'];
         $resolucion_CD = $_POST['Resolucion_CD'];
         $fecha_resolucion = $_POST['fecha_resolucion'];
 
-         $documento = $_FILES['documento'];
+        // $documento = $_FILES['documento'];
         // echo $nombre_plan ;
         // echo $asignatura ;
+
+// ---------------------------------------------------------------------------------------
+// OBTENER DATOS DE LA BASE DE DATOS
+$sqlBD = "SELECT * FROM programas WHERE id_programa = $IdProgramaForm";
+$resultadoBD = mysqli_query($conn, $sqlBD);    
+
+if (mysqli_num_rows($resultadoBD) > 0) {
+$row = $resultadoBD->fetch_assoc();
+
+}
+
+if (mysqli_num_rows($resultadoBD) > 0) {
+
+$datosBD = array(
+"Id programa" => $row["id_programa"],
+"Asignatura" => $row["id_asignatura"],
+"Id Plan" => $row["id_plan"],
+"Id Carrera" => $row["id_carrera"],
+"Cuatrimestre" => $row["cuatrimestre"],
+"Responsable" => $row["responsable"],
+"Resolucion CD" => $row["resolucion_CD"],
+"Fecha resolucion" => $row["fecha_resolucion"]
+);
+} else {
+    echo "No se encontraron resultados para el ID de plan proporcionado.";
+}
+// ---------------------------------------------------------------------------------------
+
+// Reemplazar valores si están vacíos
+if (empty($asignatura)) {
+    $asignatura = $datosBD["Asignatura"] ;
+}
+if (empty($nombre_plan)) {
+    $nombre_plan = $datosBD["Id plan"];
+}
+if (empty($nombre_carrera)) {
+    $nombre_carrera = $datosBD["Id carrera"];
+}
+if (empty($cuatrimestre)) {
+    $cuatrimestre = $datosBD["Cuatrimestre"] ;
+}
+if (empty($responsable)) {
+    $responsable = $datosBD["Responsable"] ;
+}
+if (empty($resolucion_CD)) {
+    $resolucion_CD = $datosBD["Resolucion CD"] ;
+}
+if (empty($fecha_resolucion)) {
+     $fecha_resolucion = $datosBD["Fecha resolucion"] ;
+}
+
+echo $responsable;
+
+// ------------------------------------------------------------------------------------------
+
+
+
         if (empty($_FILES['documento']) || $_FILES['documento']['error'] === UPLOAD_ERR_NO_FILE) {
+            // SI NO EXISTE UN ARCHIVO PDF INGRESA EN ESTA CONDICION
+
             // No se ha subido ningún archivo
             echo "No se ha subido ningún archivo.";
 
-            // $insertPrograma = "INSERT INTO programas 
-            // (id_asignatura, id_carrera, id_plan, cuatrimestre, responsable, resolucion_CD, fecha_resolucion)
-            //  VALUES ('$asignatura', '$nombre_carrera', '$nombre_plan', '$cuatrimestre', '$responsable', '$resolucion_CD', '$fecha_resolucion')";
-            // $resultado = mysqli_query($conn, $insertPrograma);
       
                     if (!empty($IdProgramaForm)) {
                         $updateQuery = "UPDATE programas SET 
@@ -46,15 +101,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                     if ($updateResult) {
                         // Redirigir de vuelta a la página anterior con un mensaje de éxito
-                        header("Location: {$_SERVER['HTTP_REFERER']}?mensaje=exito");
-                        
-                        
+                        header("Location: {$_SERVER['HTTP_REFERER']}?mensaje=exito");                       
                     } else {
                         echo "Error al Actualizar el programa: " . mysqli_error($conn);
                     }exit(); // Asegurar que el script se detenga después de redirigir
                 
 
         } else {
+            // SI EXISTE DATOS INFORMACION DE UN ARCHIVO PDF INFRESA EN ESTA CONDICION
+
             // TESTER DE DOCUMENTO
             // Archivo subido correctamente o hay información en el array $_FILES['documento']
             // Procede a procesar los datos del archivo, si los hay
@@ -79,15 +134,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 if (move_uploaded_file($_FILES['documento']['tmp_name'], $carpeta_destino . $nombre_archivo)) {
                     
                     // ACTUALIZAR en la base de datos
-                    
-                    // $updatePrograma = "INSERT INTO programas 
-                    // (id_asignatura, id_carrera, id_plan, cuatrimestre, 
-                    // responsable, resolucion_CD, fecha_resolucion, archivo_PDF)
-                    
-                    // VALUES ('$asignatura', '$nombre_carrera', '$nombre_plan', '$cuatrimestre',
-                    //  '$responsable', '$resolucion_CD', '$fecha_resolucion', '$nombre_archivo')";
-
-                    // $resultado = mysqli_query($conn, $updatePrograma);
 
                     if (!empty($IdProgramaForm)) {
                         $updateQuery = "UPDATE programas SET 
@@ -119,7 +165,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             } else {
                 echo "Solo se permiten archivos PDF.";
             }
-        }
+            }
 
         }   
     }
